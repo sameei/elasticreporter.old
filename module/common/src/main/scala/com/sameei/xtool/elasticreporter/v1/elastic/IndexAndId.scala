a0f45fc666d559b810ffdb93357163ddb7dec34e
Reza <reza.samei.g@gmail.com>
2018-05-18 07:47:14 -0400

    Improved IndexAndId + Test

--- module/common/src/main/scala/com/sameei/xtool/elasticreporter/v1/elastic/IndexAndId.scala
+++ module/common/src/main/scala/com/sameei/xtool/elasticreporter/v1/elastic/IndexAndId.scala
@@ -7,12 +7,18 @@ case class IndexAndId(
     idPattern: String
 ) {
 
+    val invalidChars = raw""" ${'\t'}()<>:\/*?"|,"""
+    protected val invalidCharsRegexPattern = s"[\\s${invalidChars}]"
+
     protected def generate(
         pattern: String,
         vars: Map[String, String]
     )= {
         // https://stackoverflow.com/questions/9658701
-        vars.foldLeft(pattern) { case (pt, (k,v)) => pt.replaceAll(k, Regex.quoteReplacement(v)) }
+        // https://github.com/elastic/elasticsearch/issues/6736
+        vars.foldLeft(pattern) { case (pt, (k,v)) =>
+            pt.replaceAll(k, Regex.quoteReplacement(v))
+        } replaceAll (invalidCharsRegexPattern, "-") replaceAll("(-{1,})", "-")
     }
 
     def index(vars: Map[String, String]): String = generate(indexPattern, vars)
--- module/common/src/test/scala/com/sameei/xtool/elasticreporter/v1/IndexAndIdSuite.scala
+++ module/common/src/test/scala/com/sameei/xtool/elasticreporter/v1/IndexAndIdSuite.scala
@@ -38,4 +38,25 @@ class IndexAndIdSuite extends FlatSpec with Matchers  {
 
     }
 
+    it must "replace vars properly #3" in {
+
+        // https://stackoverflow.com/questions/11913709/why-does-replaceall-fail-with-illegal-group-reference/20002683#20002683
+        // https://stackoverflow.com/questions/9658701/scala-regex-replaceallin-cant-replace-when-replace-string-looks-like-a-regex
+
+        val index = new elastic.IndexAndId("this-is-<key>", "?")
+
+        val value = "CHAIN DataSource (at org.apache.flink.api.scala.ExecutionEnvironment.fromElements(ExecutionEnvironment.scala:455) (org.apache.flink.api.java.io.CollectionInputFormat)) -> FlatMap (FlatMap at org.example.WordCount$.main(WordCount.scala:56)) -> Map (proxy-counter) -> Map (count-from-one) -> Combine(groupby-and-sum)"
+
+        val vars = Map(
+            "<key>" -> value
+        )
+
+        val rsl = index.index(vars)
+        info(rsl)
+
+        index.invalidChars.foreach { ch =>
+            rsl.find { _ == ch }.isEmpty shouldBe true
+        }
+    }
+
 }
